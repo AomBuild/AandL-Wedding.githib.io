499fc4a3b977e987aaa709ff49228f98c8d66d0f
AomBuild <208902908+AomBuild@users.noreply.github.com>
2026-07-17 08:40:38 +1000

    Wedding Website Milestone #1
    - Basic html.
    - RSVP form works.
       -> Text in tags contents keeps getting
             cleared.

---
+++ behaviour.js
@@ -0,0 +1,137 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    function InputTagCreator(ParentContainer) {
+
+        const tags = [];
+        //this is the placeholder array to fill in the values submitted from the form
+        
+
+        const tagInput = ParentContainer.querySelector(".tagInput");
+        const tagContents = ParentContainer.querySelector(".tagContents");
+        const hiddenTagContents = ParentContainer.querySelector(".hiddenTagContents");
+        const tagButton = ParentContainer.querySelector(".ADD");
+
+        const tagBoxText = ParentContainer.querySelector(".tagBoxText");
+        
+
+        
+
+
+        function addTag() {
+
+            const value = tagInput.value.trim();
+
+                if (value === "") return;
+
+                if (tags.includes(value)) {
+                    tagInput.value = "";
+                    return;
+                }
+            
+            
+            tags.push(value);
+            //this pushes the acceptable value into the 'placeholder array'
+
+            updateHiddenInput();
+            //Adds the input hidden iteration, not the html div iteration
+
+            renderTags();
+            //calls the function below to visualise that tags
+
+            tagInput.value = "";
+            //Resets the value back to the placeholder
+
+        }
+
+        function renderTags() {
+
+            // This resets -> DO NOT REMOVE
+            tagContents.innerHTML = "";
+            // tagContents.querySelectorAll(".tag").forEach(tag => tag.remove());
+
+            tags.forEach((tag, index) => {
+
+                const chip = document.createElement("div");
+
+                chip.className = "tag";
+
+                chip.innerHTML = 
+                `
+                    ${tag} <button type="button" data-index="${index}">✕</button>
+                `;
+
+                tagContents.appendChild(chip);
+
+            });
+        
+        }
+        
+
+        tagButton.addEventListener("click", addTag);
+
+        tagInput.addEventListener("keydown", (event) => {
+
+            if (event.key === "Enter") {
+
+                event.preventDefault();
+
+                addTag();
+
+            }
+        });
+
+        tagContents.addEventListener("click", (event) => {
+
+            console.log(event.target);
+
+            if (event.target.tagName !== "BUTTON") return;
+
+            const index = event.target.dataset.index;
+
+            tags.splice(index, 1);
+
+            updateHiddenInput();
+
+            renderTags();
+
+        });
+
+
+        function updateHiddenInput() {
+            hiddenTagContents.value = tags.join(", ");
+        }
+
+    }
+
+
+    InputTagCreator(document.querySelector("#dietaryRequirements"));
+    InputTagCreator(document.querySelector("#allergies"));
+
+    
+    
+    //This tackles the Google Script 
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbx8OKGxNLrdzySJbPdYj5RbjMrCCjNocHqY70voMWGg-G8rxJLOklAFUJlTwDk_axb2Ow/exec'
+
+    const form = document.forms['RSVP']
+    const submitButton = form.querySelector('input[type="submit"]');
+
+    form.addEventListener('submit', e => {
+    
+    e.preventDefault()
+
+    submitButton.disabled = true;
+    submitButton.value = "Submitting...";
+    
+    fetch(scriptURL, { 
+        method: 'POST',
+        body: new FormData(form)})
+        .then(response => alert("Thank you! Form is submitted" ))
+        .then(() => { window.location.reload(); })
+        .catch(error => console.error('Error!', error.message))
+    });
+
+});
+
+
