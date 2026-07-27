ee960216e5c44b4bdad618db85d552bc8f892bc2
AomBuild <208902908+AomBuild@users.noreply.github.com>
2026-07-28 08:29:46 +1000

    Update

--- accommodation.js
+++ accommodation.js
@@ -3,36 +3,36 @@ const accommodationCat = [
         id: 1,
         image: 'Images/Balgownie/balgownie-estate-yarra-valley_171748569914.jpg',
         heading: 'Balgownie Estate',
-        paragraph: 'This accommodation is next to the reception for ease of convience, it is also where the bridal party will be staying.',
-        map: '',
-        link: '',
+        paragraph: 'This accommodation is next to the reception, it is also where the bridal party will be staying.  <br> Use <span style="font-weight: 600;cursor">L&AWEDDINGMAR27</span> for 10% off booking.',
+        map: 'https://maps.app.goo.gl/DjGwT7h53MoMYUt77',
+        link: 'https://app.mews.com/distributor/93470a07-76a8-4b32-bde8-b2ad017e18f4',
 
     },
     {
         id: 2,
-        image: 'Images/Scene/20260710_073936.jpg',
+        image: 'Images/Healesville inn.png',
         heading: 'Healeville Hotel',
-        paragraph: 'Located in Healesville, near a lot of nice cafe, restaurants and in the heart of town.',
-        map: '',
-        link: '',
+        paragraph: 'Located in Healesville, near a lot of nice cafe and restaurants, located in the middle of the town.',
+        map: 'https://maps.app.goo.gl/6B1jNBXKbNqEiHpe7',
+        link: 'https://www.healesvillehotel.com/',
 
     },
     {
         id: 3,
-        image: 'Images/',
+        image: 'Images/TuckInn.png',
         heading: 'Tuck Inn',
-        paragraph: 'In the Heart of Healesville, recommend this place for its ease of ',
-        map: '',
-        link: '',
+        paragraph: 'Still in the town, though sligthly away from the main road, we have stayed here during one of our anniversaries to enjoy the country town. ',
+        map: 'https://maps.app.goo.gl/4sAkSJTM34Wudzyc8',
+        link: 'https://www.tuckinn.com.au/',
 
     },
     {
         id: 3,
-        image: 'Images/',
+        image: 'Images/yarraglen.png',
         heading: 'Yarra Valley grand Hotel',
-        paragraph: '2nd cloesest to the reception venue, in the center of yarra glen too so plenty of cafe, restaurants. ',
-        map: '',
-        link: '',
+        paragraph: '2nd cloesest to the reception venue, in the center of yarra glen too so plenty of cafe and restaurants with only a short distance to travel. ',
+        map: 'https://maps.app.goo.gl/q1yEk2MRDjAN7Twd7',
+        link: 'https://yarravalleygrand.com.au/',
 
     },
 
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 <div class="underline"></div>
 
-                <div style="display:flex; flex-direction:row; gap:12px; align-items: center; justify-content:space-between; width: 100%; padding: 0px 16px 8px; box-sizing: border-box; ">
+                <div style="display:flex; flex-direction:row; gap:12px; align-items: center; justify-content:space-between; width: 100%; padding: 0px 20px 16px; box-sizing: border-box; ">
                     <button class="secondary" style="width:100%;" onclick="window.open('${accommodation.map}', 'blank')"> View Maps </button>
                     <button class="primary" style="width:100%;" onclick="window.open('${accommodation.link}', 'blank')"> Book Here </button>
                 </div>
